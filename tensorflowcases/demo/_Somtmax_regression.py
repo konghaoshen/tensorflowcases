4a7ce01cad1caf0a57812e3a8cecd724871b6ff5
孔豪 <838114807@qq.com>
2019-05-14 16:01:03 +0800

    somtmax 做10分类进行手写字识别

--- tensorflowcases/demo/_Somtmax_regression.py
+++ tensorflowcases/demo/_Somtmax_regression.py
@@ -51,3 +51,8 @@ with tf.Session() as sess:
 
     # 测试
     print("TestSet acc : %s" % accuracy.eval({x: my_mnist.test.images, y: my_mnist.test.labels}))
+# 总结
+# 1，定义算法公式，也就是神经网络forward时的计算
+# 2，定义loss，选定优化器，并指定优化器优化loss
+# 3，迭代地对数据进行训练
+# 4，在测试集或验证集上对准确率进行评测
