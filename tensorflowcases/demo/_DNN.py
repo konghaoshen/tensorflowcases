20b862bdf999a41f0d0bf1644b9e357aa19ddc8b
孔豪 <838114807@qq.com>
2019-05-16 11:06:22 +0800

    自己实现fully_connected

--- tensorflowcases/demo/_DNN.py
+++ tensorflowcases/demo/_DNN.py
@@ -14,7 +14,45 @@ n_output = 10
 mnist = input_data.read_data_sets('../MNIST_data_bak/')
 X = tf.placeholder(tf.float32, shape=(None, n_inputs), name='X')
 y = tf.placeholder(tf.int32, shape=(None,), name='y')
+"""
+# 构建神经网络层，我们这里两个隐藏层，基本一样，除了输入inputs到每个神经元的连接不同
+# 和神经元个数不同
+# 输出层也非常相似，只是激活函数从ReLU变成了Softmax而已
+def neuron_layer(X, n_neurons, name, activation=None):
+    # 包含所有计算节点对于这一层，name_scope可写可不写
+    with tf.name_scope(name):
+        # 取输入矩阵的维度作为层的输入连接个数
+        n_inputs = int(X.get_shape()[1])
+        stddev = 2 / np.sqrt(n_inputs)
+        # 这层里面的w可以看成是二维数组，每个神经元对于一组w参数
+        # truncated normal distribution 比 regular normal distribution的值小
+        # 不会出现任何大的权重值，确保慢慢的稳健的训练
+        # 使用这种标准方差会让收敛快
+        # w参数需要随机，不能为0，否则输出为0，最后调整都是一个幅度没意义
+        init = tf.truncated_normal((n_inputs, n_neurons), stddev=stddev)
+        w = tf.Variable(init, name='weights')
+        b = tf.Variable(tf.zeros([n_neurons]), name='biases')
+        # 向量表达的使用比一条一条加和要高效
+        z = tf.matmul(X, w) + b
 
+        reg_loss = tf.reduce_sum(tf.abs(w))
+        reg_loss = tf.reduce_sum(tf.square(w))
+        tf.add_to_collection("reg_losses", reg_loss)
+
+        if activation == "relu":
+            return tf.nn.relu(z)
+        else:
+            return z
+
+
+with tf.name_scope("dnn"):
+    hidden1 = neuron_layer(X, n_hidden1, "hidden1", activation="relu")
+    hidden2 = neuron_layer(hidden1, n_hidden2, "hidden2", activation="relu")
+    hidden3 = neuron_layer(hidden2, n_hidden3, "hidden2", activation="relu")
+    # 进入到softmax之前的结果
+    logits = neuron_layer(hidden3, n_outputs, "outputs")
+
+"""
 with tf.name_scope('dnn'):
     #同意为with下所有的全连接层增加 L1或者L2正则项
     with tf.contrib.framework.arg_scope(
